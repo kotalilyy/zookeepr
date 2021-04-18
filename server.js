69f58569756fd52d5bad3bc83bdceb2d2ac780b9
dakota <kotalilyy@gmail.com>
2021-04-18 16:13:09 -0500

    restart local host, run tests

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { animals } = require('./data/animals');
+const express = require("express");
+const { animals } = require("./data/animals");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,35 +8,41 @@ function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
   if (query.personalityTraits) {
-    if (typeof query.personalityTraits === 'string') {
+    if (typeof query.personalityTraits === "string") {
       personalityTraitsArray = [query.personalityTraits];
     } else {
       personalityTraitsArray = query.personalityTraits;
     }
-    personalityTraitsArray.forEach(trait => {
+    personalityTraitsArray.forEach((trait) => {
       filteredResults = filteredResults.filter(
-        animal => animal.personalityTraits.indexOf(trait) !== -1
+        (animal) => animal.personalityTraits.indexOf(trait) !== -1
       );
     });
   }
   if (query.diet) {
-    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.diet === query.diet
+    );
   }
   if (query.species) {
-    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
   }
   if (query.name) {
-    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
   }
   return filteredResults;
 }
 
 function findById(id, animalsArray) {
-  const result = animalsArray.filter(animal => animal.id === id)[0];
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
 }
 
-app.get('/api/animals', (req, res) => {
+app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
@@ -44,7 +50,7 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
-app.get('/api/animals/:id', (req, res) => {
+app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
